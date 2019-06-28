b59c313c4eb7a9166f1f69552716983c2b65918f
Manoj <manoj.sakthivel@firstsynch.com>
2019-06-28 14:14:02 +0530

    2 nd commit maxvalue

---
+++ maxvalue.py
@@ -0,0 +1,18 @@
+
+
+
+def mano1():
+    num = int(input('how many nos '))
+    max_num = 0
+    for n in range(num):
+        number = int(input('Enter the no '))
+        if max_num < number:
+            max_num = number
+    print('the max value is ',max_num)
+        
+
+mano1()
+
+
+
+#print(list(range(10)))
